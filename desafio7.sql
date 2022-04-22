f1ef26e3e00eeb35953f9b50826bbc398122e884
gemaquejr <gemaquejr@hotmail.com>
2022-04-22 01:27:11 -0300

    Adiciona fix2 requisito 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,9 @@
-SELECT ar.artist_name AS artista,
+SELECT
+ar.artist_name AS artista,
 al.album_name AS album,
 COUNT(fo.artist_id) AS seguidores
 FROM SpotifyClone.artist AS ar
 INNER JOIN SpotifyClone.album AS al
-ON ar.artist_id = al.artist_id
+ON al.artist_id = ar.artist_id
 INNER JOIN SpotifyClone.following_artist AS fo
-ON fo.artist_id = ar.artist_id GROUP BY ar.artist_name, al.album_name ORDER BY seguidores DESC, ar.artist_name, al.album_name;
+ON fo.artist_id = ar.artist_id GROUP BY ar.artist_name, al.album_name ORDER BY seguidores DESC, artista, album;
